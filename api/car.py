532cdc1259c5c8647ebd7ec0f8901ee1ab7c4443
Aemilian <aemilian.mayrhofer@gmail.com>
2020-05-06 08:29:29 +0200

    added base PWM steering

--- api/car.py
+++ api/car.py
@@ -1,8 +1,14 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-channel = 25
-GPIO.setup(channel, GPIO.OUT)
+drive_channel = 22 # GPIO25
+steer_channel = 12 # GPIO18
+GPIO.setup(drive_channel, GPIO.OUT)
+GPIO.setup(steer_channel, GPIO.OUT)
+steer = GPIO.PWM(steer_channel, 50)
+steer.start(0)
+max_rot = 104.4
 
 class Car:
     def __init__(self):
@@ -10,18 +16,26 @@ class Car:
 
     def accelerate(self):
         print('accelerate')
-        GPIO.output(channel, True)
+        GPIO.output(drive_channel, True)
 
     def slow(self):
         print('break')
-        GPIO.output(channel, False)
+        GPIO.output(drive_channel, False)
 
     def steer(self, amount):
+        target = max_rot * amount / 100
+        duty = target / 18 + 2
+        GPIO.output(steer_channel, True)
+        steer.ChangeDutyCycle(duty)
+        sleep(15)
+        GPIO.output(steer_channel, False)
+
         print(f'steer {amount}')
 
     def toggle_ss(self):
         self.lights = not self.lights
         print(f'lights are {self.lights}')
 
-    def cleanup(self):
-        GPIO.cleanup(channel)
+    def __del__(self):
+        GPIO.cleanup(drive_channel)
+        GPIO.cleanup(steer_channel)
