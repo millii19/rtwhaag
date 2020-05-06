a0a0fd7bc8e0ca412b81ccd8ab0feb99f7b9ce9a
Aemilian <aemilian.mayrhofer@gmail.com>
2020-05-06 09:21:32 +0200

    created steer-loop

--- api/car.py
+++ api/car.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import threading
+
+
+    
 
 GPIO.setmode(GPIO.BOARD)
 drive_channel = 22 # GPIO25
@@ -14,6 +18,12 @@ class Car:
     def __init__(self):
         self.lights = False
         self.amount = 50
+        self.running = True
+        self.thread = None
+    
+    def start():
+        self.thread = threading.Thread(target=self.steer_loop)
+        self.thread.start()
 
     def accelerate(self):
         print('accelerate')
@@ -26,20 +36,33 @@ class Car:
     def steer(self, amount):
         if amount is self.amount:
             return
-        target = max_rot * amount / 100
-        duty = target / 18 + 2
-        GPIO.output(steer_channel, True)
-        steer.ChangeDutyCycle(duty)
-        sleep(0.5)
-        GPIO.output(steer_channel, False)
+        self.amount = amount
+        
 
-        print(f'steer {amount}')
+    def _steer_loop(self):
+        local_amt = self.amount
+        while self.running:
+            if local_amt is self.amount:
+                sleep(0.5)
+                continue
+            else
+                local_amt = self.amount
+            target = max_rot * self.amount / 100
+            duty = target / 18 + 2
+            GPIO.output(steer_channel, True)
+            steer.ChangeDutyCycle(duty)
+            sleep(0.5)
+            GPIO.output(steer_channel, False)
 
     def toggle_ss(self):
         self.lights = not self.lights
         print(f'lights are {self.lights}')
 
     def cleanup(self):
+        self.running = False
+        print('waiting for steer loop')
+        self.thread.join()
+        print('joined')
         GPIO.output(drive_channel, False)
         GPIO.cleanup(drive_channel)
         GPIO.output(steer_channel, False)
