edd4b23ee99b332b49f577eeb894379272a8b84b
Aemilian <aemilian.mayrhofer@gmail.com>
2020-06-24 19:11:30 +0200

    added a test API
    for testing without pi access

---
+++ api/server-test.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/toggle_lights')
+def toggle_lights():
+    return f'lights are {car.lights}'
+
+@app.route('/steer/<int:target>')
+def steer(target):
+    return f'steered {target}'
+
+@app.route('/accelerate')
+def accelerate():
+    return 'accelerate'
+
+@app.route('/break')
+def slow():
+    return 'break'
+
+@app.route('/toggle/<int:pin>', methods=['GET'])
+def toggle(pin):
+    return f'Toggled pin {pin}'
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
---
+++ api/start-test.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=server-test.py
+flask run --host=0.0.0.0
